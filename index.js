9e94fc3790cebacf6a5194511c35f303c08f032c
Wendy Dherin <wendydherin@gmail.com>
2018-12-09 20:49:50 +0000

    Create custom handler for i18n view.

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
+'use strict'
+
+const Accept = require('accept')
 const Hapi = require('hapi')
 const Path = require('path')
 
-const server = new Hapi.Server()
+const server = new Hapi.Server({
+  app: {
+    i18n: {
+      supportedLangs: ['en', 'fr', 'zh'],
+      defaultLang: 'en'
+    }
+  }
+})
+
 server.connection({ host: 'localhost', port: 4000 })
 
 server.register(require('vision'), (err) => {
@@ -16,12 +27,30 @@ server.register(require('vision'), (err) => {
     path: Path.join(__dirname, 'templates')
   })
 
+  server.handler('i18n-view', (route, options) => {
+    const view = options.view
+
+    return function (request, reply) {
+      const settings = server.settings.app.i18n
+      const langs = Accept.languages(request.headers['accept-language'])
+
+      for (let i = 0; i < langs.length; ++i) {
+        if (settings.supportedLangs.indexOf(langs[i] !== -1)) {
+          return reply.view(view + '_' + langs[i])
+        }
+      }
+      reply.view(view + '_' + defaultLanguage)
+    }
+  })
+
   server.route([
     {
       method: 'GET',
       path: '/',
       handler: {
-        view: 'index'
+        'i18n-view': {
+          view: 'index'
+        }
       }
     }
   ])
